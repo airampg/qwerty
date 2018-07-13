fc0b96a53c2667f64ea215267f3271ee2d64f738
airampg <airpegui@hotmail.com>
2018-07-13 20:33:10 +0100

    primer commit

---
+++ ImRich3/ImRich3/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ImRich3
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            DisplayAlert("Enhorabuena", "Ahora nosotros tenemos tu dinero :)", "cerrar >:-(");
+        }
+
+        void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            double value = args.NewValue;
+            rotatingLabel.Rotation = value;
+            displayLabel.Text = String.Format("The Slider value is {0}", value);
+        }
+    }
+}
